44202225ae50fe400a42596d1609d875dd8a31ac
hung.dongxuan <dongxuanhung2596@gmail.com>
2017-11-27 14:58:27 +0700

    update Tab Details

--- component/Details.js
+++ component/Details.js
@@ -8,7 +8,10 @@ import {
     ScrollView,
 } from 'react-native';
 import Slide from './Slide'
+import {TabBar} from "../Routes";
 import firebase from '../config/firebaseconfig'
+import { Rating } from 'react-native-elements';
+import ViewMoreText from 'react-native-view-more-text';
 
 export default class Details extends Component<{}>{
 
@@ -18,20 +21,29 @@ export default class Details extends Component<{}>{
     }
 
     render(){
-        let star = {
-            uri: 'http://www.potters.com.au/wp-content/uploads/2017/09/five-stars.png'
-        }
         return(
             <View style={details.container}>
-                <ScrollView>
+                <ScrollView contentContainerStyle={{borderWidth: 0, flex: 1}}>
                     <Slide array={this.props.navigation.state.params.images_slide}/>
                     <View style={details.title}>
-                        <Text style={{fontSize:18, color:'white', fontWeight:'bold'}}>{this.props.navigation.state.params.name}</Text>
-                        <Text style={details.text}>{this.props.navigation.state.params.short_desc}</Text>
+                        <Text style={{fontSize:24, color:'white', fontWeight:'bold'}}>{this.props.navigation.state.params.name}</Text>
+                        <ViewMoreText numberOfLines={2}
+                                      renderViewMore={this.renderViewMore}
+                                      renderViewLess={this.renderViewLess}>
+                            <Text style={details.text}>{this.props.navigation.state.params.short_desc}</Text>
+                        </ViewMoreText>
                     </View>
                     <View style={{justifyContent:'center', alignItems:'center'}}>
-                        <Image source={star} style={{height:50, width:200}}></Image>
+                        <Rating
+                            type="star"
+                            fractions={1}
+                            imageSize={35}
+                            startingValue={3.5}
+                            onFinishRating={this.ratingCompleted}
+                            style={{ marginTop:5 }}
+                        />
                     </View>
+                    <TabBar/>
                 </ScrollView>
             </View>
         );
@@ -42,14 +54,14 @@ export default class Details extends Component<{}>{
 const details = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor:'white'
     },
     title:{
         backgroundColor:'deepskyblue',
         padding:5,
-        height: 70
     },
     text:{
         color:'white',
-        fontSize: 16,
+        fontSize: 18,
     }
 });
---
+++ component/ScreenTabDetails/ServiceDetails.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class ServiceDetails extends Component {
+    render() {
+        return (
+            <Text>Service Screen</Text>
+        );
+    }
+}
